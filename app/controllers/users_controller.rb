64cd56ae1d54fa377ed19942a5eca8b61f7c0852
Gary Nuñez <gnunezc@unsa.edu.pe>
2019-05-09 04:08:30 -0500

    Edit and controller User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,42 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+ 
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    redirect_to(:action =>'index')
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
-
+ 
   def create
-  end
-
-  def update
+    @user = User.new(user_params)
+    @user.save
+    redirect_to(:action => 'index')
   end
 
   def delete
+    @user = User.find(params[:id])
+  end
+ 
+ 
+  def destroy
+    @user = Task.find(params[:id])
+    @user.destroy
+    redirect_to(:action => 'index')
   end
 
   private
 
   def user_params
-    params.fetch(:user, {}).permit(:first_name, :last_name, :email)
+    params.fetch(:user, {}).permit(:first_name, :last_name, :phone, :email, :encrypted_password)
   end
 end
