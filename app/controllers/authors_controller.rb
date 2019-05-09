81df74409f76d9928b2f31e28f45f138bd2283bc
josecaros <jchanila@unsa.edu.pe>
2019-05-08 19:01:03 -0500

    update de author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Author.all
   end
 
   def edit
@@ -15,6 +16,15 @@ class AuthorsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+        if @author.update(authors_params)
+          format.html { redirect_to @author, notice: 'El autor ha sido correctamente actualizado.' }
+          format.json { render :show, status: :ok, location: @estudiante }
+        else
+          format.html { render :edit }
+          format.json { render json: @author.errors, status: :unprocessable_entity }
+        end
+    end
   end
 
   def delete
