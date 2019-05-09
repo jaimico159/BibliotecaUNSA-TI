caf1ff79baeb3213e17f391fe46a698dd1bd0dd5
yhecarpio <ycarpiore@unsa.edu.pe>
2019-05-09 03:52:07 -0500

    Primer commit authors_controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Author.all
   end
 
   def edit
@@ -9,15 +10,41 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def create
+    @author = Author.new(author_params)
+    @author.save
+    respond_to do |format|
+      if @author.save
+        format.html { redirect_to @author, notice: 'Author was succesfully created' }
+        format.json { render :show, status: :created, location: @author }
+      else
+        format.html { render :new }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @author.update(author_params)
+        format.html { redirect_to @author, notice: 'Author was successfully updated.' }
+        format.json { render :show, status: :ok, location: @author }
+      else
+        format.html { render :edit }
+        format.json { render json: @author.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def delete
+    @author.delete
+    respond_to do |format|
+      format.html { redirect_to authors_url, notice: 'Author was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
 
   private
